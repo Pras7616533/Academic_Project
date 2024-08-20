85d6c4147d10c7b0dbcddc85e5a38232ee5c07a6
Pras7616533 <143571194+Pras7616533@users.noreply.github.com>
2024-08-20 08:26:27 +0530

    Update array_manager.c

--- C_ project/Teach_data/CID/array_manager.c
+++ C_ project/Teach_data/CID/array_manager.c
@@ -1,10 +1,10 @@
 /*
-   Made by Prashant Deshmukh no 16/08/2024
-   roll no : 2258
+   Made by Prashant Deshmukh on 16/08/2024
+   Roll No: 2258
 */
 
 #include <stdio.h>
-//#include <conio.h>
+#include <conio.h>
 #include <stdlib.h>
 #define MAX 100
 
@@ -17,99 +17,86 @@ void display();
 int main() {
     char name[100];
     int ch;
-    system("clear");
-//    clrscr();
+    clrscr();
 
-    printf("\n\tEnter you name: ");
-    fgets(name, sizeof(name), stdin);
-//    gets(name);
-    system("clear");
-//    clrscr();
+    printf("\n\tEnter your name: ");
+    gets(name);
+    clrscr();
     setbuf(stdout, NULL);
-    printf("\n\t\t\t\t*** Array Mene ***");
-  //  printf("\n1.create\n2.insert\n3.delete\n4.display\n5.exit\n");
-    while(1)
-    {
-        printf("\n\tEnter the your choice:\n\t1.create\n\t2.insert\n\t3.delete\n\t4.display\n\t5.exit\n\t\t==> ");
-        scanf("%d",&ch);
-        switch(ch)
-        {
+
+    while(1) {
+        printf("\n\t\t\t\t*** Array Menu ***\n\tEnter your choice:\n\t1. Create\n\t2. Insert\n\t3. Delete\n\t4. Display\n\t5. Exit\n\t\t==> ");
+        scanf("%d", &ch);
+
+        switch(ch) {
             case 1:
-            create();
-            printf("\tArray is Created by %s \n", name);
-            break;
+                create();
+                printf("\tArray is created by %s\n", name);
+                break;
 
             case 2:
-            insert();
-            printf("\tElement is Inserted by %s\n", name);
-            break;
+                insert();
+                printf("\tElement is inserted by %s\n", name);
+                break;
 
             case 3:
-            delet();
-            printf("\tElement is Deleted by %s\n", name);
-            break;
+                delet();
+                printf("\tElement is deleted by %s\n", name);
+                break;
 
             case 4:
-            display();
-            break;
+                display();
+                break;
 
             case 5:
-            printf("\n\tThank You! %s... ", name);
-            exit(0);
-            break;
+                printf("\n\tThank you! %s...", name);
+                exit(0);
+                break;
 
             default:
-            printf("\n\tWront Choice!!");
+                printf("\n\tWrong choice!!");
         }
     }
 }
 
-void create()
-{
+void create() {
     int i;
-    printf("\tEnter tne value of n: ");
-    scanf("%d",&num);
-    printf("\tEnter %d element: ", num);
-    for (i = 0; i < num; i++)
-    {
-        scanf("%d",&a[i]);
+    printf("\n\t\t\t\t*** Array Creating ***\n\tEnter the value of n: ");
+    scanf("%d", &num);
+    printf("\tEnter %d elements: ", num);
+    for (i = 0; i < num; i++) {
+        scanf("%d", &a[i]);
     }
 }
 
-void insert()
-{
+void insert() {
     int i, ele, ind;
-    printf("\tEnter tne value for insert: ");
-    scanf("%d",&ele);
+    printf("\n\t\t\t*** Inserting Array Element ***\n\tEnter the value to insert: ");
+    scanf("%d", &ele);
     printf("\tEnter index after which element to be inserted: ");
-    scanf("%d",&ind);
-    for (i = num-1; i >= ind; i--)
-    {
-       a[i+1] = a[i];
+    scanf("%d", &ind);
+    for (i = num - 1; i >= ind; i--) {
+        a[i + 1] = a[i];
     }
     num++;
     a[ind] = ele;
 }
 
-void delet()
-{
+void delet() {
     int i, ind;
-    printf("\tEnter tne value for deleted: ");
-    scanf("%d",&ind);
-    for (i = ind + 1; i <= ind; i++)
-    {
-       a[i-1] = a[i];
+    printf("\n\t\t\t*** Deleting Array Element ***\n\tEnter the index of the element to be deleted: ");
+    scanf("%d", &ind);
+    for (i = ind + 1; i <= num; i++) {
+        a[i - 1] = a[i];
     }
     num--;
 }
 
-void display()
-{
+void display() {
     int i;
-    printf("\n\t\t\t\t***** Elements in Array *****.\n\t\t");
-    for (i = 0; i < num; i++)
-    {
-       printf("%d ",a[i]);
+    printf("\n\t\t\t***** Elements in Array *****\n\t");
+    for (i = 0; i < num; i++) {
+        printf("%d ", a[i]);
     }
-    printf("\n\n");
+    printf("\n");
 }
